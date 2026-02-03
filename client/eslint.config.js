66e1c3a322f7de0555da10250ac23bd3d7471dcb
Udit Narayan Singh <147933844+Uditsingh5@users.noreply.github.com>
2026-02-04 04:03:02 +0530

    Initial commit - Zentra

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -26,4 +26,8 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]', argsIgnorePattern: '^_' }],
     },
   },
+  {
+    files: ['vite.config.js'],
+    languageOptions: { globals: globals.node },
+  },
 ])
